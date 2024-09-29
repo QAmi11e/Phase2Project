021d048d92a6b609fffce2406d73fbd5dc519bad
Camille's PC <camille.soliman@rfsmart.com>
2024-09-29 10:24:35 -0400

    API Get and POST requests

--- tests/API Tests/api.spec.js
+++ tests/API Tests/api.spec.js
@@ -1,6 +1,23 @@
-import {test,expect} from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-test("GET Store Inventory", async ({request}) => {
-    const response = await request.get("https://petstore.swagger.io/v2/store/inventory");
+test("GET User 1's Post", async ({ request }) => {
+    const response = await request.get("https://jsonplaceholder.typicode.com/posts/1");
     expect(response.status()).toBe(200);
+    const textResponse = await response.text();
+    expect(textResponse).toContain('1');
 });
+
+test("POST New Comment", async ({ request }) => {
+    const response = await request.post("https://jsonplaceholder.typicode.com/posts", {
+        data: {
+            "userId": 11,
+            "id": 101,
+            "title": "Wubba Lubba Dub Dub",
+            "body": "Such is the burden of life."
+        }
+    })
+    expect(response.status()).toBe(201);
+    const textResponse = await response.text();
+    expect(textResponse).toContain('11');
+    console.log(await response.json());
+})
